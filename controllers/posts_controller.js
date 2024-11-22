992ec1b944d70b90fca4064b68913824a8d9c12f
Raz Alon <71208052+razalon72@users.noreply.github.com>
2024-11-22 11:08:41 +0200

    add get post to post rout (#3)

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -2,3 +2,13 @@ import Post from "../models/posts_model.js";
 export async function addPost(post) {
     return await Post.create(post);
 }
+
+export async function getPosts(filters) {
+    const query = {};
+
+    if (filters.sender) {
+        query.sender = filters.sender.toString();
+    }
+
+    return await Post.find(query);
+}
