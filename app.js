41c787876dc9afdda5c0ff0a4a8bda1fbb943581
Rom Cohen <romco@Roms-MacBook-Air.local>
2024-11-22 10:42:10 +0200

    adding skelton db connection

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-const express = require("express");
-const app = express();
-const dotenv = require("dotenv").config();
-const port = process.env.PORT;
+import express from "express";
+import bodyParser from "body-parser";
+import postRouter from "./routes/posts.js";
 
-const mongoose = require("mongoose");
-mongoose.connect(process.env.DB_CONNECT);
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to database"));
+export function createApp() {
+    const app = express();
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use("/posts", postRouter);
 
-const postsRoute = require("./routes/posts_route");
-app.use("/posts", postsRoute);
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+    return app;
+}
